23e2675f0a87a53d9a897234d5090a78164d8d70
Prince Goswami <princenath0757@gmail.com>
2026-03-29 17:27:16 +0530

    I have started developing the backend for the Contact Store System and have already created some initial files and structure.

---
+++ src/main/java/com/mycompany/contactStore/ContactStoreApp.java
@@ -0,0 +1,13 @@
+package com.mycompany.contactStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactStoreApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactStoreApp.class, args);
+	}
+
+}
